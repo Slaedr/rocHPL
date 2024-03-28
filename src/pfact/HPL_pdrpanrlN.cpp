9b5029682b2bd18b82371c0c7c55a24932349644
Aditya Kashi <aditya.kashi@mail.mcgill.ca>
2024-03-28 10:56:49 -0400

    trsm uses up to all available threads

--- src/pfact/HPL_pdrpanrlN.cpp
+++ src/pfact/HPL_pdrpanrlN.cpp
@@ -160,7 +160,7 @@ void HPL_pdrpanrlN(HPL_T_panel* PANEL,
 
     //const int omp_task_size = get_task_size(panel->nb, jb);
     const int omp_task_size = 16;
-    const int trsm_thread_size = 7;
+    const int trsm_thread_size = thread_size;
 
     // make sure the diagonal subblock factorization is completed
 #pragma omp barrier
